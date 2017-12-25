362aa38198df5bc67896404c7923eafa2999b37f
tigerk0430 <gyurse@gmail.com>
2017-12-26 00:49:04 +0900

    from windows

--- python_archive/pygta5/pygta5_p1.py
+++ python_archive/pygta5/pygta5_p1.py
@@ -7,15 +7,17 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
-import time
 
-last_time = time.time()
-
+# 무한루프를 돌면서 
 while(True):
+    # (0,40)부터 (800,600)좌표까지 창을 만들어서 데이터를 저장하고 screen 변수에 저장합니다
     screen = np.array(ImageGrab.grab(bbox=(0,40,800,600)))
 
-    cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    # pygta5-1이라는 이름의 창을 생성하고 이 창에 screen 이미지를 뿌려줍니다
+    cv2.imshow('pygta5-1', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+
+    # 'q'키를 누르면 종료합니다
     if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindwos()
+        cv2.destroyAllWindows()
         break
 
