993c5f660625a1b31902b7d5e475cc5a49de7cc2
lockdpwn <gyurse@gmail.com>
2018-02-06 00:45:36 +0900

    from linux : Ubuntu 16.04.1 LTS

--- zet_cetera/ChangeBackgroundCode(not_used).bat
+++ zet_cetera/ChangeBackgroundCode(not_used).bat
@@ -1,5 +1,4 @@
 
-
 reg add "HKCU\Control Panel\Desktop" /v Walpaer /f /t REG_SZ /d C:\Users\admin\Desktop\4.jpg
 
 copy /Y C:\Users\admin\Desktop\4.jpg C\Users\admin\Appdata\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper.jpg
