ab1e9e96e7f9ef4102a3a2573adc765b08c21bb2
tigerk0430 <gyurse@gmail.com>
2018-03-17 15:04:24 +0900

    180317_2

--- python_archive/test.py
+++ python_archive/test.py
@@ -4,25 +4,40 @@
 	python ==> 케라스딥러닝강화학습 p
 '''
 
-from keras.models import Sequential
-from keras.layers.core import Dense, Lambda
-from keras.layers.embeddings import Embedding
-import keras.backend as K
+from gensim.models import word2vec
+import logging
+import os
 
 
-vocab_size = 5000
-embed_size = 300
-window_size = 1
-text = "I love green eggs and ham"
+class Text8Sentences(object):
+	def __init__(self, fname, maxlen):
+		self.fname = fname
+		self.maxlen = maxlen
 
 
-model = Sequential()
-model.add(Embedding(input_dim=vocab_size, output_dim=embed_size,
-					embeddings_initializer='glorot_uniform',
-					input_length=window_size*2))
+	def __iter__(self):
+		with open(os.path.join(DATA_DIR, "text8"), "rb") as ftext:
+			text = ftext.read().split(" ")
+			words =[]
 
-model.add(Lambda(lambda x: K.mean(x, axis=1), output_shape=(embed_size,)))
-model.add(Dense(vocab_size, kernel_initializer='glorot_uniform',
-				activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+			for word  in text :
+				if len(words) >= self.maxlen:
+					yield words
+					words = []
+				words.append(word)
+			yield words
 
+
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+# DATA_DIR = "../data"
+DATA_DIR = "/media/dyros-vehicle/edward_6/"
+sentences = Text8Sentences(os.path.join(DATA_DIR, "text8"), 50)
+model = word2vec.Word2Vec(sentences, size=300, min_count=30)
+
+model.most_similar("woman")
+model.most_similar(positive=['woman', 'king'], negative=['man'])
+
+# ed: model 결과벡터를 정규화하고 저장한다
+model.init_sims(replace=True)
+model.save("word2vec_gensim.bin")
