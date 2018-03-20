da25bab03cf6a48d27fd8e2c43e46ee79fc1f327
lockdpwn <gyurse@gmail.com>
2018-03-20 16:13:00 +0900

    from linux : Ubuntu 16.04.4 LTS

--- zet_cetera/ctrl_caps_change.sh
+++ zet_cetera/ctrl_caps_change.sh
@@ -1,2 +1,8 @@
-dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:swapcaps']"
-
+case $1 in
+    original)
+        dconf write /org/gnome/desktop/input-sources/xkb-options "['']"
+        ;;
+    *)
+        dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:swapcaps']"
+        ;;
+esac
