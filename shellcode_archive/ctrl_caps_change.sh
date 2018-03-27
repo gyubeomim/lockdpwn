684f339b62dfa84cb5ff0bc88e34746071055deb
tigerk0430 <gyurse@gmail.com>
2018-03-27 14:52:21 +0900

    from linux : Ubuntu 16.04.4 LTS

--- shellcode_archive/ctrl_caps_change.sh
+++ shellcode_archive/ctrl_caps_change.sh
@@ -3,8 +3,10 @@
 case $1 in
     original)
         dconf write /org/gnome/desktop/input-sources/xkb-options "['']"
+        dconf read /org/gnome/desktop/input-sources/xkb-options
         ;;
     *)
         dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:swapcaps']"
+        dconf read /org/gnome/desktop/input-sources/xkb-options
         ;;
 esac
