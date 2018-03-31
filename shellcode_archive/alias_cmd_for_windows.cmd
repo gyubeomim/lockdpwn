5f07b18248799f427275255256c07847b800b394
tigerk0430 <gyurse@gmail.com>
2018-03-31 20:10:54 +0900

    from MSI Windows 10 x64

---
+++ shellcode_archive/alias_cmd_for_windows.cmd
@@ -0,0 +1,17 @@
+@echo off
+REM doskey 명령어를 사용해 cmd에서 bash 같은 alias 명령어를 사용하는 코드
+
+doskey ls=dir
+doskey ll=dir /w
+doskey l=dir /w
+
+doskey gr=cd e:\gitrepo\
+doskey git1=cd e:\gitrepo\lockdpwn
+doskey git2=cd e:\gitrepo\ims
+doskey git3=cd e:\gitrepo\ims_ml
+
+doskey up=cd c:\Users\edward\
+doskey down=cd c:\Users\edward\Downloads
+doskey cd=cd c:\Users\edward
+
+
