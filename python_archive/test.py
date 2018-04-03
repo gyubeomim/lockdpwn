0f888882b4609a1b7f428a08d155ec8f8d368511
tigerk0430 <gyurse@gmail.com>
2018-04-03 23:46:00 +0900

    180403_2

--- python_archive/test.py
+++ python_archive/test.py
@@ -1,27 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-    python ==> 자동차융합실험1, 전자전기 회로 이론 및 센서 설계 실험의 그래프를 그리는 코드
+    python ==> dyros, [x,y,z,intensity] 필드를 가지고 있는 veloydne_point cloud 데이터를 numpy, pandas로 가공해본
 '''
 
 import numpy as np
-import matplotlib.pyplot as plt
-
-# plt.title('5V, 10V source Voltage [Red, Blue]')
-# plt.plot([100, 200, 500], [0.0001, 0.0002, 0.0005], 'ro')
-# plt.plot([100, 200, 500], [0.0002, 0.0004, 0.0010], 'bo')
-# plt.xlabel('mass [g]')
-# plt.ylabel('output Voltage [mV]')
-# plt.grid(True)
-# plt.show()
+import pandas as pd
 
 
-plt.plot([11.4, 6.3, 4.34, 3.42], [11.33, 12.52, 12.99, 13.2], 'ro')
-plt.plot([11.31,6.26,4.32,3.30], [11.31,12.52,12.96,13.2], 'bo')
-plt.ylabel('Voltage [V]')
-plt.xlabel('Current [mA]')
-plt.grid(True)
-plt.show()
+data = open('velodyne_points.dat', 'r').read().split('\n')
+
+# ed: create (0,0,0,0)
+data_np = np.zeros(4)
+
+for i in range(0, 10000):
+    data_np = np.vstack((data_np, data[i].split(', ')))
+
+    if(i % 2000 == 0):
+        print(i)
 
 
+# ed: remove first zeros (0,0,0,0)
+data_np = data_np[1:len(data_np)].astype(np.float)
 
+data_pd = pd.DataFrame(data=data_np)
+
+
+data_pd.info()
+data_pd.hist()
+data_pd.sum()
+data_pd.describe()
+
+
+#-------------------------------------------------------------
+
+data2 = open('Grid_JW_1.txt', 'r').read().split('\n')
+
+# ed: create (0,0,0,0)
+data_np2 = np.zeros(8)
+
+for i in range(0, 10000):
+    data_np2 = np.vstack((data_np2, data2[i].split(', ')))
+
+    if(i % 2000 == 0):
+        print(i)
+
+
+# ed: remove first zeros (0,0,0,0)
+data_np2 = data_np2[1:len(data_np2)].astype(np.float)
+
+data_pd2 = pd.DataFrame(data=data_np2)
+
+
+data_pd2.info()
+data_pd2.hist()
+data_pd2.sum()
+data_pd2.describe()
