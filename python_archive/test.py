9f116437a72c378c089a413dbc4e7375e0b20e1d
tigerk0430 <gyurse@gmail.com>
2018-04-16 16:06:52 +0900

    from MSI Windows 10 x64

--- python_archive/test.py
+++ python_archive/test.py
@@ -1,58 +1,28 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import os
+
+image_set_file = os.path.join('../data/ImageSet/train.txt')
+
+with open(image_set_file) as f:
+   image_idx = [x.strip() for x in f.readlines()]
+
+perm_idx = [image_idx[i] for i in
+      np.random.permutation(np.arange(len(image_idx)))]
+
+BATCH_SIZE = 32
+cur_idx = 0
+
+batch_idx = perm_idx[cur_idx:cur_idx + BATCH_SIZE]
+
+for idx in batch_idx:
+	print(idx)
 '''
-    python ==> dyros, [x,y,z,intensity] 필드를 가지고 있는 veloydne_point cloud 데이터를 numpy, pandas로 가공해본
+
+result :
+2011_09_26_0056_0000000126
+2011_09_26_0011_0000000223
+2011_09_26_0009_0000000407
+2011_09_26_0020_0000000065
+2011_09_26_0022_0000000797
+...
+
 '''
-
-import numpy as np
-import pandas as pd
-
-
-data = open('velodyne_points.dat', 'r').read().split('\n')
-
-# ed: create (0,0,0,0)
-data_np = np.zeros(4)
-
-for i in range(0, 10000):
-    data_np = np.vstack((data_np, data[i].split(', ')))
-
-    if(i % 2000 == 0):
-        print(i)
-
-
-# ed: remove first zeros (0,0,0,0)
-data_np = data_np[1:len(data_np)].astype(np.float)
-
-data_pd = pd.DataFrame(data=data_np)
-
-
-data_pd.info()
-data_pd.hist()
-data_pd.sum()
-data_pd.describe()
-
-
-#-------------------------------------------------------------
-
-data2 = open('Grid_JW_1.txt', 'r').read().split('\n')
-
-# ed: create (0,0,0,0)
-data_np2 = np.zeros(8)
-
-for i in range(0, 10000):
-    data_np2 = np.vstack((data_np2, data2[i].split(', ')))
-
-    if(i % 2000 == 0):
-        print(i)
-
-
-# ed: remove first zeros (0,0,0,0)
-data_np2 = data_np2[1:len(data_np2)].astype(np.float)
-
-data_pd2 = pd.DataFrame(data=data_np2)
-
-
-data_pd2.info()
-data_pd2.hist()
-data_pd2.sum()
-data_pd2.describe()
