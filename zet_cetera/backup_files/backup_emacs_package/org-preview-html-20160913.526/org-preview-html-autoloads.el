7ceadde353b7b6f57425bb207ab98de190d24229
tigerk0430 <gyurse@gmail.com>
2018-04-25 16:59:32 +0900

    180425

---
+++ zet_cetera/backup_files/backup_emacs_package/org-preview-html-20160913.526/org-preview-html-autoloads.el
@@ -0,0 +1,27 @@
+;;; org-preview-html-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "org-preview-html" "org-preview-html.el" (23263
+;;;;;;  14438 566267 125000))
+;;; Generated autoloads from org-preview-html.el
+
+(autoload 'org-preview-html/preview "org-preview-html" "\
+Export current org-mode buffer to a temp file and call `eww-open-file' to preview it.
+
+\(fn)" t nil)
+
+(autoload 'org-preview-html-mode "org-preview-html" "\
+Preview current org file in eww whenever you save it.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; org-preview-html-autoloads.el ends here
---
+++ zet_cetera/backup_files/backup_emacs_package/org-preview-html-20160913.526/org-preview-html-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-preview-html" "20160913.526" "automatically use eww to preview the current org file on save" '((org "8.0") (emacs "24.4")) :commit "2b6afbaf1fcf781bcd53074ee62895f15f5282e5" :keywords '("convenience" "eww" "org") :url "https://github.com/lujun9972/org-preview-html")
