6e41ad879704f7d091b7a7abe5f6c73cace26125
tigerk0430 <gyurse@gmail.com>
2018-04-25 18:33:19 +0900

    from linux : Ubuntu 16.04.4 LTS

--- python_archive/STEM/dyros_data_analysis.py
+++ python_archive/STEM/dyros_data_analysis.py
@@ -5,14 +5,30 @@ import matplotlib.pyplot as plt
 
 
 #START============================================
-# ed: dyros_#.txt 파일 분석 코드
+# ed: dyros_#.txt 파일을 불러오는 코드
 dyros_np = np.loadtxt('./dyros_002_1.txt', delimiter=',')
 dyros_pd = pd.DataFrame(dyros_np)
 #END==============================================
 
 
 #START============================================
-# ed: pred_cls 변수에 value 별로 갯수를 세주는 코드
-unique, counts = np.unique(pred_cls, return_counts=True)
+# ed: dyros_np 변수에 value 별로 갯수를 세주는 코드
+unique, counts = np.unique(dyros_np, return_counts=True)
 dict(zip(unique, counts))
 #END==============================================
+
+
+
+#START============================================
+# ed: dyros_#.txt 파일에서 [0,0,0,0,0,0,0,0]의 비율을 검사하는 코드
+# Label Count : 8714
+# 모든 Pointcloud   선택했다
+dyros_np = np.loadtxt('./dyros_002_15.txt', delimiter=',')
+dyros_pd = pd.DataFrame(dyros_np)
+
+unique, counts = np.unique(dyros_np[:,:3], return_counts=True)
+(counts.sum()-counts.max()) / float(counts.sum())
+#END==============================================
+
+
+
