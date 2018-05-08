d11285b532f48013870f6090482737eac157242b
tigerk0430 <gyurse@gmail.com>
2018-05-09 01:05:37 +0900

    test.cpp updated

--- cpp_archive/test.cpp
+++ cpp_archive/test.cpp
@@ -4,32 +4,16 @@
 #include <array>
 
 int main(int argc, char **argv){
-  int oldStyle[3] = {1, 4, 3};
-  int anotherOld[3];
-
-  std::memcpy(anotherOld, oldStyle, sizeof(int)*3);
-  std::sort(oldStyle, oldStyle, 3);
-
-  for (auto i : oldStyle) {
-    std::cout << i << ' ';
-  }
-
-  bool dif = false;
-  for (int i=0 ; i<3 ; i) {
-    if (oldStyle[i] != anotherOld[i]) {
-      dif = true;
-      break;
-    }
-  }
-
-  std::array<int,3> c11Style = {1,4,3};
-  std::array<int,3> anotherC11;
-
+  std::array<int,10> c11Style = {1,4,3,123,5,2,6,62,24,1523};
+  std::array<int,10> anotherC11;
   anotherC11 = c11Style;
+
+  std::sort(c11Style.begin(), c11Style.end());
 
   for (auto i : c11Style) {
     std::cout << i << ' ';
   }
+  std::cout << std::endl;
 
   bool diff = (c11Style != anotherC11);
   return 0;
