478696e88cf9132fa5d83fe8a4743b6c3968dcf5
tigerk0430 <gyurse@gmail.com>
2018-05-15 17:06:49 +0900

    c++ code update

---
+++ cpp_archive/3_BOJ/boj_11725_2.cpp
@@ -0,0 +1,60 @@
+/*
+  c++ ==> 백준 11725, 트리의 부모 찾기를 BFS로 푼 정답 코드
+
+
+  code from [http://hellogohn.com/post_one230]
+*/
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#define MAX_SIZE 10001
+
+using namespace std;
+
+int parent[MAX_SIZE] = {0};
+vector<int> node[MAX_SIZE];
+int total_num;
+
+void bfs() {
+  queue<pair<int, int>> bfs_queue;
+
+  for(int i=0; i < node[1].size(); i++) {
+    bfs_queue.push({1, node[1].at(i)});
+  }
+
+  while(bfs_queue.size()) {
+    pair<int, int> sub_ = bfs_queue.front();
+    bfs_queue.pop();
+
+    parent[sub_.second] = sub_.first;
+
+    for(int i=0; i<node[sub_.second].size(); i++) {
+      if(parent[node[sub_.second].at(i)] == 0) {
+        bfs_queue.push({sub_.second, node[sub_.second].at(i)});
+      }
+    }
+  }
+}
+
+int main(int argc, char **argv) {
+  cin >> total_num;
+
+  for(int i=0; i<total_num-1 ; i++) {
+    int sub_1, sub_2;
+    cin >> sub_1 >> sub_2;
+
+    node[sub_1].push_back(sub_2);
+    node[sub_2].push_back(sub_1);
+  }
+
+  parent[1] = -1;
+  bfs();
+
+  cout << "--------------------------------" << endl;
+  for(int i=2; i<=total_num; i++) {
+    cout << parent[i] << endl;
+  }
+
+  return 0;
+}
--- python_archive/test.py
+++ python_archive/test.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
-	python ==> fibonacci 수열을 python 코드를 통해 구현한 코드
-'''
-fibo = {0:0, 1:1}
+from pythonds.graphs import Graph
 
-def fibonacci(n):
-	global fibo
-
-	fibo.setdefault(n, 0)
-	if n >= 2:
-		for i in range(2, n+1):
-			fibo[i] = fibo[i-1] + fibo[i-2]
-
-
-idx = int(input("[+] Please type any integer : "))
-fibonacci(idx)
-
-print(fibo[idx])
-print(fibo)
+class DFSGraph(Graph):
