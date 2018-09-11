24fdc2aef7fabeb44c50623a2159a20c4b468bfd
edward0im <gyurse@gmail.com>
2018-09-11 22:13:06 +0900

    180911

--- cpp_archive/test.cpp
+++ cpp_archive/test.cpp
@@ -1,7 +1,26 @@
-/*
- * #+DESCRIPTION:
- *
- *
- * #+DATE:        2018-07-14-토
- * #+AUTHOR:      Edward Im (gyurse@gmail.com)
- */
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+struct TEST{
+    double t1;
+    double t2;
+    double t3;  
+};
+
+int main() {
+    TEST t;
+
+    for(int i=0; i<2; i++) {
+        stringstream ss;
+        ss << 't' << i;
+        string var_ = ss.str();
+
+        t.var_ = i*10;
+        cout << t.var_ << endl;
+    }
+
+    return 0;
+}
