e3bf86fab66b20522f5afda15b263b63d90e6816
tigerk0430 <gyurse@gmail.com>
2018-12-02 20:29:37 +0900

    from MSI Windows 10 x64

--- script_archive/alias_cmd_for_windows.cmd
+++ script_archive/alias_cmd_for_windows.cmd
@@ -8,9 +8,9 @@ doskey ll=dir /w
 doskey l=dir /w
 
 doskey gr=cd e:\gitrepo 
-doskey git1=cd e:\gitrepo\lockdpwn 
-doskey git2=cd e:\gitrepo\ims 
-doskey git3=cd e:\gitrepo\ims_ml 
+doskey g1=cd e:\gitrepo\lockdpwn 
+doskey g2=cd e:\gitrepo\ims_ros
+doskey g3=cd e:\gitrepo\ims_carto
 
 doskey up=cd c:\Users\edward 
 doskey down=cd c:\Users\edward\Downloads 
