480fec2a04390046b4c80de0b6f660735daef783
edward0im <gyurse@gmail.com>
2018-12-16 14:08:32 +0900

    .emacs_w: changed for applying in Windows

--- matlab_archive/test.m
+++ matlab_archive/test.m
@@ -1,19 +1,21 @@
-clear all ; close all ; clc;
+clear; close all; clc; 
 
-
-t = 0 : 0.001 : 1;
-p = 1;
-appr_rect = 1/2;
-figure;
-
-for p = 1:1:1000
-    appr_rect = appr_rect+ 2*(1./((2*p-1)*pi).*sin(2*pi*(2*p-1)*t));
-    plot(t, appr_rect)
-    num_of_P = num2str(2*p-1);
-    T = strcat('+ 1/', num_of_P, '\pi*sin(', num_of_P,'+\pit)');
-    text(0.7, 0.25, T);
-    pause(0.1);
-end
-
-axis([-0.1 1.1 -0.1 1.1]);
-grid on;
+rng(0, 'twister');
+% Problem declaration n = 5;
+PG = zeros(n); end R0 = eye(3); % nominal attitude
+Rtrue = zeros(3, 3, n); ttrue = zeros(3, 1, n); for i = 1 : n
+Rtrue(:, :, i) = R0; ttrue(:, :, i) = [cos(2*pi*i/n), sin(2*pi*i/n), 0]'; end
+% SDP
+MQQ = zeros(3*n); MQT = zeros(3*n, n); MTT = zeros(n); for i = 1 : n - 1 for j = i + 1 : n if PG(i, j) == 1 ei = zeros(n, 1); ei(i) = 1; ej = zeros(n, 1); ej(j) = 1; Ei = kron(ei, eye(3)); Ej = kron(ej, eye(3));
+ptb1 = 0.1*randn(3, 1); ptb2 = 0.01*randn(3, 1); Rij = expm([0 -ptb1(3) ptb1(2);
+ptb1(3) 0 -ptb1(1); -ptb1(2) ptb1(1) 0]); tij = ttrue(:,:,j) ttrue(:,:,i) + ptb2; MQQ = MQQ - 1/2*(Ei*Rij*Ej' +
+Ej*Rij'*Ei');
+MQT = MQT - Ei*tij*(ej-ei)'; MTT = MTT + (ej - ei)*(ej ei)'; end end end M = [MQQ MQT; MQT' MTT];
+cvx_begin SDP variable Lambda0(3, 3, n) symmetric; Lambda = Lambda0(:, :, 1); for i = 2 : n
+Lambda = blkdiag(Lambda,
+Lambda0(:, :, i)); end
+maximize(trace(Lambda)) subject to 0 <= M - blkdiag(Lambda, zeros(n));
+cvx_end
+Lambda2 = full(Lambda); X = null(M - blkdiag(Lambda2, zeros(n)));
+% pose graph
+for i = 1 : n - 1 PG(i, i + 1) = 1;
