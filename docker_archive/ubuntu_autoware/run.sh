074809bb27531009f9388074e1ddeee1a4dd0a07
lockdpwn <gyurse@gmail.com>
2019-01-06 16:51:46 +0900

    docker archive for autoware updated

--- docker_archive/ubuntu_autoware/run.sh
+++ docker_archive/ubuntu_autoware/run.sh
@@ -1,7 +1,18 @@
+#!/bin/sh
+XSOCK=/tmp/.X11-unix
+XAUTH=/home/$USER/.Xauthority
+
 docker run \
-           --runtime=nvidia \
-		   --net=host \
-		   --name autoware_docker \
-		   --it \
-		   -v /home/dyros-vehicle/docker:/root/data \
-		   edward0im/dyrosvehicle:ubuntu_autoware
+    --runtime=nvidia \
+    -it \
+    --net=host \
+	--name autoware_docker \
+    --env="XAUTHORITY=${XAUTH}" \
+    --env="DISPLAY=${DISPLAY}" \
+    -v $XSOCK:$XSOCK:rw \
+    -v $XAUTH:$XAUTH:rw \
+    -v $HOST_DIR:$SHARED_DIR:rw \
+    --privileged -v /dev/bus/usb:/dev/bus/usb \
+	-v /home/dyros-vehicle/docker:/home/autoware/docker \
+	-v /media/dyros-data/bag_files:/home/autoware/bag_files \
+	edward0im/dyrosvehicle:ubuntu_autoware
--- docker_archive/ubuntu_cuda9.0/run.sh
+++ docker_archive/ubuntu_cuda9.0/run.sh
@@ -3,5 +3,6 @@ docker run \
 		   --net=host \
 		   --name segmap_docker \
 		   -it \
-		   -v /home/dyros-vehicle/docker:/root/data \
+		   -v /home/dyros-vehicle/docker:/root/docker \
+		   -v /media/dyros-data/bag_files:/root/bag_files \
 		   edward0im/dyrosvehicle:ubuntu_cuda9.0
