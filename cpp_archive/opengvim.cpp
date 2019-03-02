bb86cf0cc2d731269fea06edcd6ca909c0ea178a
edward im <gyurse@gmail.com>
2019-03-02 21:16:53 +0900

    <2019-03-02 Sat 21:16>

---
+++ cpp_archive/opengvim.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+system("nircmd exec hide gvim_x64.exe");
+return 0;
+}
--- script_archive/alias_cmd_for_windows.cmd
+++ script_archive/alias_cmd_for_windows.cmd
@@ -1,25 +1,24 @@
 @echo off
 REM doskey 명령어를 사용해 cmd에서 bash 같은 alias 명령어를 사용하는 코드
 
-doskey uptime=powershell uptime2.ps1
-
 doskey ls=dir
 doskey ll=dir /w
 doskey l=dir /w
 
-doskey gr=cd e:\gitrepo 
-doskey g1=cd e:\gitrepo\lockdpwn 
-doskey g2=cd e:\gitrepo\ims_ros
-doskey g3=cd e:\gitrepo\ims_carto
-
-doskey up=cd c:\Users\edward 
-doskey down=cd c:\Users\edward\Downloads 
+doskey gr=cd e:\gitrepo\
+doskey git1=cd e:\gitrepo\lockdpwn
+doskey git2=cd e:\gitrepo\ims
+doskey git3=cd e:\gitrepo\ims_ml
 
 doskey ipy=cd e:\gitrepo\lockdpwn\python_archive\ipython
 doskey ml=cd e:\gitrepo\lockdpwn\python_archive\machineLearning
 
-doskey tf=activate tf
-doskey vi=cd c:\Program Files (x86)\Vim 
+doskey up=cd c:\Users\edward\
+doskey down=cd c:\Users\edward\Downloads
 
-doskey notebook=ipython notebook
+doskey tf=activate tf
 doskey qt=ipython qtconsole
+doskey notebook=ipython notebook
+
+doskey gv="C:\Program Files (x86)\Vim\vim81_x64\gvim_x64.exe"
+
