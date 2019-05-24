3055ceea8ef8e76f8077f8990d18d60e13c841b8
edward im <gyurse@gmail.com>
2019-05-25 02:21:53 +0900

    from Windows

--- cpp_archive/test_win.cpp
+++ cpp_archive/test_win.cpp
@@ -24,21 +24,10 @@
 
 using namespace std;
 
-int d[1001];
 
 int main(int argc, char const *argv[]) {
   int n;
   cin >> n;
-
-  d[0] = 1;
-  d[1] = 1;
-
-  for(int i=2; i<=n; i++) {
-    d[i] = d[i-1] + d[i-2];
-    d[i] %= 10007;
-  }
-
-  cout << d[n] << '\n';
 
   return 0;
 }
