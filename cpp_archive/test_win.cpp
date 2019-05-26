d876eefe1ee3b46b907b6e6dafe85a4e5ad0b26f
edward im <gyurse@gmail.com>
2019-05-26 20:22:36 +0900

    from Windows

--- cpp_archive/test_win.cpp
+++ cpp_archive/test_win.cpp
@@ -19,39 +19,75 @@
 
 
 
+
+
 #include <cstdio>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-using LL = long long;
-
-LL d[1001][10];
-
+int d[10001];
+int A[10001];
+int sum[10001];
 
 int main(int argc, char const *argv[]) {
   int n;
   cin >> n;
 
-  d[1][0] = 0;
-  d[1][1] = 1;
+  for (int i = 1; i <= n; i++) {
+    scanf("%d", &A[i]);
+  }
 
-  for (int i = 2; i <= n; i++) {
-    for (int j = 0; j <= 1; j++) {
-        if (j == 0)
-          d[i][0] = d[i - 1][0] + d[i-1][1];
-        else {
-          d[i][1] = d[i-1][0]; 
+  for (int i = 1; i <= n; i++) {
+    d[i] = 1;
+    sum[i] = A[i];
+  }
+
+  vector<int> v;
+  vector<vector<int>> vec;
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j < i; j++) {
+      if (A[i] > A[j]) {
+        d[i] = std::max({d[i], d[j] + 1});
+        v.push_back(j);
       }
     }
+    if(i==1)
+      v.push_back(1);
+
+    vec.push_back(v);
+    if (!v.empty()) v.clear();
   }
 
-  LL result = 0;
-  for (int i = 0; i <= 9; i++) {
-    result += d[n][i];
+  for(int i=2; i<=vec.size(); i++) {
+    for(int j=1; j<=vec.at(i-1).size(); j++)
+      sum[i] += A[vec.at(i-1).at(j-1)];
   }
 
-  cout << result << '\n';
+  // cout << "---------------------" << endl;
+  // for (auto it : vec) {
+  //   for (auto it2 : it) {
+  //     cout << it2 << " ";
+  //   }
+  //   cout << endl;
+  // }
+  // cout << "---------------------" << endl;
+  // for (auto it : d) {
+  //   if (it != 0)
+  //     cout << it << " ";
+  // }
+  // cout << endl;
+
+  // for (auto it : sum) {
+  //   if (it != 0)
+  //     cout << it << " ";
+  // }
+  // cout << endl;
+
+  cout << *std::max_element(sum, sum + n + 1) << '\n';
 
   return 0;
 }
