1aa9f8b4f95a705d38bbffd4d87404c04cc9f8cc
edward im <gyurse@gmail.com>
2019-05-29 16:36:03 +0900

    from Windows

--- cpp_archive/test_win.cpp
+++ cpp_archive/test_win.cpp
@@ -20,7 +20,6 @@
 
 
 
-
 #include <cstdio>
 #include <iostream>
 #include <vector>
@@ -28,66 +27,28 @@
 
 using namespace std;
 
-int d[10001];
-int A[10001];
-int sum[10001];
+using LL = long long;
+
+LL d[100001];
+LL A[100001];
 
 int main(int argc, char const *argv[]) {
   int n;
   cin >> n;
 
-  for (int i = 1; i <= n; i++) {
-    scanf("%d", &A[i]);
+  for(int i=0; i<n; i++) {
+    cin >> A[i];
   }
 
-  for (int i = 1; i <= n; i++) {
-    d[i] = 1;
-    sum[i] = A[i];
+
+  d[0] = A[0];
+
+  for(int i=1; i<n; i++) {
+    d[i] = std::max(d[i], d[i - 1] + A[i]);
   }
 
-  vector<int> v;
-  vector<vector<int>> vec;
-
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j < i; j++) {
-      if (A[i] > A[j]) {
-        d[i] = std::max({d[i], d[j] + 1});
-        v.push_back(j);
-      }
-    }
-    if(i==1)
-      v.push_back(1);
-
-    vec.push_back(v);
-    if (!v.empty()) v.clear();
-  }
-
-  for(int i=2; i<=vec.size(); i++) {
-    for(int j=1; j<=vec.at(i-1).size(); j++)
-      sum[i] += A[vec.at(i-1).at(j-1)];
-  }
-
-  // cout << "---------------------" << endl;
-  // for (auto it : vec) {
-  //   for (auto it2 : it) {
-  //     cout << it2 << " ";
-  //   }
-  //   cout << endl;
-  // }
-  // cout << "---------------------" << endl;
-  // for (auto it : d) {
-  //   if (it != 0)
-  //     cout << it << " ";
-  // }
-  // cout << endl;
-
-  // for (auto it : sum) {
-  //   if (it != 0)
-  //     cout << it << " ";
-  // }
-  // cout << endl;
-
-  cout << *std::max_element(sum, sum + n + 1) << '\n';
+  cout << *std::max_element(d, d+n-1) << '\n';
 
   return 0;
 }
+
