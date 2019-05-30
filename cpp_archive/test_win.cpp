73b3c58c2a7cfb30ffc2c1f2f4ec5159960967d2
edward im <gyurse@gmail.com>
2019-05-31 01:47:31 +0900

    from Windows

--- cpp_archive/test_win.cpp
+++ cpp_archive/test_win.cpp
@@ -33,21 +33,28 @@ LL d[100001];
 LL A[100001];
 
 int main(int argc, char const *argv[]) {
-  int n;
-  cin >> n;
+  int T,n;
+  cin >> T;
 
-  for(int i=0; i<n; i++) {
-    cin >> A[i];
+  int t = 1;
+
+  d[1] = 1;
+  d[2] = 1;
+  d[3] = 1;
+  d[4] = 2;
+  d[5] = 2;
+
+  while(t <= T) {
+    cin >> n;
+
+    for (int i = 6; i <= n; i++) {
+      d[i] = d[i - 1] + d[i - 5];
+    }
+
+    cout << d[n] << '\n';
+
+    t += 1;
   }
-
-
-  d[0] = A[0];
-
-  for(int i=1; i<n; i++) {
-    d[i] = std::max(d[i], d[i - 1] + A[i]);
-  }
-
-  cout << *std::max_element(d, d+n-1) << '\n';
 
   return 0;
 }
