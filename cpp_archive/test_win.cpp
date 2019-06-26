6001b947704205440ef7af53862d2724bb077223
edward im <gyurse@gmail.com>
2019-06-26 16:14:45 +0900

    from Windows

--- cpp_archive/test_win.cpp
+++ cpp_archive/test_win.cpp
@@ -3,39 +3,46 @@
 #include <cstdio>
 #include <cstring>
 #include <iostream>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
-int n, e, chk[1001], gph[1001][1001];
+int v, chk[1001], gph[1001][1001];
 
 void dfs(int now) {
   chk[now] = 1;
-  for(int i=1; i<=n; i++) {
-    if (!chk[i] && gph[now][i]) {
+
+  for(int i=1; i<=v; i++) {
+    if(!chk[i] && gph[now][i]) {
       dfs(i);
     }
   }
 }
 
 int main() {
-  cin >> n >> e;
+  int T;
+  cin >> T;
 
-  for(int i=0; i<e; i++) {
-    int x,y;
-    cin >> x >> y;
-    gph[x][y] = gph[y][x] = 1;
-  }
+  while(T--) {
+    cin >> v;
 
-  int count = 0;
-  for(int i=1; i<=n;i++){
-    if(!chk[i]) {
-      dfs(i);
-      count += 1;
+    int x;
+    for(int i=1; i<=v; i++) {
+      scanf("%d ", &x);
+      gph[i][x] = gph[x][i] = 1;
     }
+
+    int result = 0;
+
+    for(int i=1; i<=v; i++) {
+      if(!chk[i]) {
+        dfs(i);
+        result += 1;
+      }
+    }
+    cout << result << '\n';
+    memset(chk, 0, sizeof(chk));
+    memset(gph, 0, sizeof(gph));
   }
-
-  cout << count << '\n';
-
   return 0;
 }
