7194597a1e5ec43333e727dbc71a1302f2f0d62b
edward im <gyurse@gmail.com>
2019-07-07 01:28:41 +0900

    from Windows

--- cpp_archive/test_w.cpp
+++ cpp_archive/test_w.cpp
@@ -22,46 +22,46 @@ void dfs(int x, int y, int num) {
   for (int d = 0; d < 4; d++) {
     int nx = x + dx[d];
     int ny = y + dy[d];
-    if (chk(nx, ny) && map[nx][ny] && !visited[nx][ny])
+
+    if (chk(nx, ny) && map[nx][ny] && !visited[nx][ny]) {
       dfs(nx, ny, num);
+    }
   }
 }
 
 int bfs(int num) {
-	queue<pair<int,int>> Q;
-	memset(visited,0,sizeof(visited));
+  queue<pair<int, int>> Q;
+  memset(visited, 0, sizeof(visited));
 
-	for(int i=0;i<N;i++){
-		for(int j=0;j<N;j++){
-			if(map[i][j]==num){
-				visited[i][j]=1;
-				Q.push({i,j});
-			}
-		}
-	}
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
+      if (map[i][j] == num) {
+        visited[i][j] = true;
+        Q.push({i, j});
+      }
 
-	int result = 0;
-	while(!Q.empty()) {
-		int qsize = Q.size();
-		while(qsize--){
-			int x=Q.front().first;
-			int y=Q.front().second;
-			Q.pop();
+  int result = 0;
+  while (!Q.empty()) {
+    int qsize = Q.size();
+    while (qsize--) {
+      int x = Q.front().first;
+      int y = Q.front().second;
+      Q.pop();
 
-			for(int d=0;d<=4;d++){
-				int nx=x+dx[d];
-				int ny=y+dy[d];
+      for (int d = 0; d < N; d++) {
+        int nx = x + dx[d];
+        int ny = y + dy[d];
 
-				if(!chk(nx,ny)) continue;
-				if(map[nx][ny] !=0 && map[nx][ny]!=num) return result;
-				if(map[nx][ny]==0&&!visited[nx][ny]){
-					visited[nx][ny]=1;
-					Q.push({nx,ny});
-				}
-			}
-		}
-		result+=1;
-	}
+        if (!chk(nx, ny)) continue;
+        if (map[nx][ny] != 0 && map[nx][ny] != num) return result;
+        if (map[nx][ny] == 0 && !visited[nx][ny]) {
+          visited[nx][ny] = true;
+          Q.push({nx, ny});
+        }
+      }
+    }
+    result += 1;
+  }
 }
 
 int main() {
@@ -75,19 +75,17 @@ int main() {
     }
   }
 
-	for(int i=0;i<N;i++) {
-		for(int j=0;j<N;j++){
-			if(map[i][j] && !visited[i][j])
-			dfs(i,j,++num);
-		}
-	}
-
-	int min_dist = 9999;
-
-	for(int i=1;i<=num;i++){
-		min_dist = std::min(min_dist,bfs(i));
-	}
-	cout << min_dist << '\n';
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
+      if (map[i][j] && !visited[i][j]) {
+        num += 1;
+        dfs(i, j, num);
+      }
+  int dmin = 9999;
+  for (int i = 1; i <= num; i++) {
+    dmin = min(dmin, bfs(i));
+  }
+  cout << dmin << '\n';
 
   return 0;
 }
