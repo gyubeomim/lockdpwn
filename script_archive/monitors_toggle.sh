5d00c9cb869f5854d1e27defd5bed3045f64fa5d
tigerk0430 <gyurse@gmail.com>
2019-08-30 19:15:17 +0900

    20190830 : Ubuntu 16.04.6 LTS

--- script_archive/monitors_toggle.sh
+++ script_archive/monitors_toggle.sh
@@ -19,10 +19,10 @@ case $1 in
         ;;
     atlas1)
 		# 2개 모니터를 ON
-       xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1080x420 --output DVI-I-3 --mode 1920x1080 --pos 0x0 --rotate right
+       xrandr --output DVI-I-2 --primary --mode 1920x1080 --pos 1920x0 --output DVI-I-3 --mode 1920x1080 --pos 0x0 
         ;;
     off_atlas1)
 		# 1개 모니터만 켠다 
-       xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0  --output DVI-I-3 --off
+       xrandr --output DVI-I-2 --primary --mode 1920x1080 --pos 0x0  --output DVI-I-3 --off
         ;;
 esac
