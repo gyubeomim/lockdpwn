05c3ef917fab270313bdc45d22ca440ba710164e
tigerk0430 <gyurse@gmail.com>
2019-09-03 09:48:54 +0900

    20190903 : Ubuntu 16.04.6 LTS

--- cmake_projects/direct_method_example/include/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil . (
-         (cmake-ide-project-dir . "../")
-         (cmake-ide-build-dir . "./build")
-         (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Release  -DCMAKE_PREFIX_PATH=/usr/local/lib;/opt/ros/kinetic")
-         )))
