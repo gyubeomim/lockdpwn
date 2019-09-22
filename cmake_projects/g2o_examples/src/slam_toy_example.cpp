8be5e1b337e469cba83bd9e47194ae588574e7f8
edward <gyurse@gmail.com>
2019-09-23 08:33:13 +0900

    20190923 : Ubuntu 16.04.6 LTS

--- cmake_projects/g2o_examples/src/slam_toy_example.cpp
+++ cmake_projects/g2o_examples/src/slam_toy_example.cpp
@@ -67,68 +67,49 @@ int main(int argc, const char* argv[]){
   optimizer->setAlgorithm(solver);
 
   vector<Eigen::Isometry3d, aligned_allocator<Eigen::Isometry3d>> true_poses;
-  cout << "debug01" << endl;  // ed: DEBUG
-
   int vertex_id=0;
-
 
   for(size_t i=0; i<5; i++){
     Vector3d trans(i*0.04-1, 0 ,0);
-    Vector3d trans2(i*0.04+1, 0 ,0);
     Eigen::Quaterniond q;
     q.setIdentity();
-    cout << "debug3" << endl;  // ed: DEBUG
 
     Eigen::Isometry3d pose;
     pose.setIdentity();
     pose.linear() = q.toRotationMatrix();
     pose.translation() = trans;
 
-    Eigen::Isometry3d prev_pose;
-    prev_pose.setIdentity();
-    prev_pose.linear() = q.toRotationMatrix();
-    prev_pose.translation() = trans2;
-
-    g2o::VertexSE3* prev_v_se3 = new g2o::VertexSE3();
     g2o::VertexSE3* v_se3 = new g2o::VertexSE3();
     v_se3->setId(vertex_id);
 
-    cout << "debug3" << endl;  // ed: DEBUG
     if(i==0){
       v_se3->setFixed(true);
     }
 
     v_se3->setEstimate(pose);
     optimizer->addVertex(v_se3);
+    cout << "debug01" << endl;  // ed: DEBUG
+
     true_poses.push_back(pose);
-
-    cout << "debug4" << endl;  // ed: DEBUG
-    if(i>=0) {
-      prev_v_se3->setId(vertex_id+5);
-      prev_v_se3->setEstimate(prev_pose);
-      optimizer->addVertex(prev_v_se3);
-
-      Eigen::Isometry3d relative_pose = pose.inverse() * prev_pose;
-
-      cout << "debug5" << endl;  // ed: DEBUG
-      g2o::EdgeSE3* e_se3(new g2o::EdgeSE3());
-      e_se3->setMeasurement(relative_pose);
-      e_se3->setInformation(Eigen::MatrixXd::Identity(6,6));
-      // e_se3->setVertex(0, dynamic_cast<g2o::OptimizableGraph::Vertex*>(prev_v_se3));
-      // e_se3->setVertex(1, dynamic_cast<g2o::OptimizableGraph::Vertex*>(v_se3));
-      e_se3->vertices()[0] = prev_v_se3;
-      e_se3->vertices()[1] = v_se3;
-      optimizer->addEdge(e_se3);
-      cout << "debug6" << endl;  // ed: DEBUG
-    }
-    // prev_pose = pose;
     vertex_id += 1;
   }
 
-  cout << "debug7" << endl;  // ed: DEBUG
+  for(int i=1;i<true_poses.size();i++){
+    g2o::EdgeSE3* e_se3(new g2o::EdgeSE3());
+    Eigen::Isometry3d relative_pose = true_poses.at(i-1).inverse() * true_poses.at(i);
+    e_se3->setMeasurement(relative_pose);
+    e_se3->setInformation(Eigen::MatrixXd::Identity(6,6));
+    e_se3->vertices()[0] = optimizer->vertex(i-1);
+    e_se3->vertices()[1] = optimizer->vertex(i);
+    e_se3->setId(i);
+    optimizer->addEdge(e_se3);
+    cout << "debug2" << endl;  // ed: DEBUG
+  }
+
+    cout << "debug3" << endl;  // ed: DEBUG
   optimizer->initializeOptimization();
   optimizer->setVerbose(true);
   optimizer->optimize(10);
-  cout << "debug8" << endl;  // ed: DEBUG
+    cout << "debug4" << endl;  // ed: DEBUG
 
 }
