9c7ade412b85e60bc617953ae5586a9d9fbb479e
edward <gyurse@gmail.com>
2019-10-31 22:25:05 +0900

    20191031 : Ubuntu 16.04.6 LTS

---
+++ docker_archive/ubuntu_cuda9.0/run-edward.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+XSOCK=/tmp/.X11-unix
+XAUTH=/home/$USER/.Xauthority
+
+xhost +local:docker
+
+docker run \
+	--runtime=nvidia \
+	--net=host \
+	--name docker_cuda9.0 \
+	-it \
+	--env="XAUTHORITY=${XAUTH}" \
+	--env="DISPLAY=unix$DISPLAY" \
+	--env="XDG_RUNTIME_DIR=/run/user/1000" \
+	--privileged  \
+	-v /run/user/1000:/run/user/1000 \
+	-v /dev/bus/usb:/dev/bus/usb \
+	-v $XSOCK:$XSOCK:rw \
+	-v $XAUTH:$XAUTH:rw \
+	-v /media/data/gitrepo/dv_ws/dyros_ws/docker_ws/src_ubuntu_cuda9.0:/root/docker_ws/src \
+	-v /media/data/gitrepo/dv_ws/dyros_ws/docker_ws/not_used_ubuntu_cuda9.0:/root/docker_ws/not_used \
+	-v /home/edward/share_docker:/root/share_docker \
+	-v /media/data/bag_files:/root/bag_files \
+	--expose 22 \
+	edward0im/dyrosvehicle:ubuntu_cuda9.0
---
+++ script_archive/run_openvslam.sh
@@ -0,0 +1,4 @@
+./run_kitti_slam \
+	-v /media/data/gitrepo/openvslam/orb_vocab.dbow2 \
+	-d /media/data/dataset/kitti/data_odometry/sequences/00 \
+	-c /media/data/gitrepo/openvslam/example/kitti/KITTI_mono_00-02.yaml 
