1536ca7ebacc865cf3e639ef85e93dadd1c521e3
tigerk0430 <gyurse@gmail.com>
2019-11-08 22:13:43 +0900

    20191108 : Ubuntu 16.04.6 LTS

--- cmake_projects/mathlib/src/mathlib.cc
+++ cmake_projects/mathlib/src/mathlib.cc
@@ -45,46 +45,5 @@ double GetMagnitudeAndNormalize(Eigen::Matrix3d& X) {
   return magnitude;
 }
 
-namespace SO3 {
-Eigen::Matrix3d so3(Eigen::Vector3d w) {
-  return skew(w);
-}
-
-// https://en.wikipedia.org/wiki/3D_rotation_group
-Eigen::Matrix3d exp(Eigen::Matrix3d so3) {
-  Eigen::Matrix3d SO3;
-
-  double theta = GetMagnitudeAndNormalize(so3);
-
-  double s = std::sin(theta);
-  double c = std::cos(theta);
-
-  double s2 = std::sin(theta)*std::sin(theta);
-  double c2 = std::cos(theta)*std::cos(theta);
-
-  double x = so3(2,1);
-  double y = so3(0,2);
-  double z = so3(1,0);
-
-  double x2 = x*x;
-  double y2 = y*y;
-  double z2 = z*z;
-
-  SO3(0,0) = 2*(x2-1) * s2 + 1;
-  SO3(0,1) = 2*x*y*s2 - 2*z*c*s;
-  SO3(0,2) = 2*x*z*s2 + 2*y*c*s;
-
-  SO3(1,0) = 2*x*y*s2 + 2*z*c*s;
-  SO3(1,1) = 2*(y2-1)*s2 + 1;
-  SO3(1,2) = 2*y*z*s2 - 2*x*c*s;
-
-  SO3(2,0) = 2*x*z*s2 - 2*y*c*s;
-  SO3(2,1) = 2*y*z*s2 + 2*x*c*s;
-  SO3(2,2) = 2*(z2-1)*s2 + 1;
-
-  return SO3;
-}
-}  // namespace SO3
-
 
 } // namespace mathlib
