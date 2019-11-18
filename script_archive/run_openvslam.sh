e0e8776e4d1ca6efdfbd04fe1a43e87030fca6d7
edward <gyurse@gmail.com>
2019-11-19 01:11:04 +0900

    20191119 : Ubuntu 16.04.6 LTS

--- script_archive/run_openvslam.sh
+++ script_archive/run_openvslam.sh
@@ -1,4 +1,20 @@
-/media/data/gitrepo/openvslam-sandbox/build/run_kitti_slam \
-	-v /media/data/gitrepo/openvslam-sandbox/orb_vocab.dbow2 \
-	-d /media/data/dataset/kitti/data_odometry/sequences/00 \
-	-c /media/data/gitrepo/openvslam-sandbox/example/kitti/KITTI_mono_00-02.yaml
+
+
+case $1 in
+	0)
+		/media/data/gitrepo/openvslam-sandbox/build/run_kitti_slam \
+			-v /media/data/gitrepo/openvslam-sandbox/orb_vocab.dbow2 \
+			-d /media/data/dataset/kitti/data_odometry/sequences/00 \
+			-c /media/data/gitrepo/openvslam-sandbox/example/kitti/KITTI_mono_00-02.yaml
+		;;
+	1)
+		/media/data/gitrepo/openvslam-sandbox/build/run_video_slam \
+			-v /media/data/gitrepo/openvslam-sandbox/orb_vocab.dbow2 
+		;;
+	ros)
+		source ~/gitrepo/openvslam-sandbox/ros/devel/setup.bash
+		rosrun openvslam run_slam \
+		-v /media/data/gitrepo/openvslam-sandbox/orb_vocab.dbow2 \
+		-c /media/data/gitrepo/openvslam-sandbox/example/dyros_half.yaml
+		;;
+esac
