03b9003936c0b52ead1fd2f1fefdbd26b84ccb4f
tigerk0430 <gyurse@gmail.com>
2019-12-18 22:16:17 +0900

    20191218 : Ubuntu 16.04.6 LTS

--- script_archive/monitors_toggle.sh
+++ script_archive/monitors_toggle.sh
@@ -3,7 +3,7 @@
 case $1 in
     off)
        # 중앙 모니터만 ON 나머지는 OFF
-       xrandr --output eDP1 --off --output VGA1 --mode 1920x1080R --pos 0x0 --output HDMI1 --off
+       xrandr --output eDP1 --off --output VGA1 --mode 1920x1080R --off --output HDMI1 --pos 0x0 --scale 1.0x1.0
         ;;
     three1)
        # 3개 모니터를 전부 Horizontal하게 ON
