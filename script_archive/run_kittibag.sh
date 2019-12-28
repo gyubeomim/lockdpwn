9b0a501915c9a7707b34e1fe1a24fe9f7f7679ba
edward im <gyurse@gmail.com>
2019-12-28 23:54:32 +0900

    20191228 : Ubuntu 16.04.6 LTS

--- script_archive/run_kittibag.sh
+++ script_archive/run_kittibag.sh
@@ -1,1 +1,10 @@
-rosbag play ${HOME}/bag_files/kitti/kitti_odometry_sequence_00.bag 	kitti/camera_color_right/image:=/camera/right/image_rect kitti/camera_color_left/image:=/camera/left/image_rect kitti/camera_color_right/camera_info:=/camera/right/camera_info kitti/camera_color_left/camera_info:=/camera/left/camera_info kitti/camera_gray_left/image:=/camera/left/image_rect_gray
+
+if [ -z "$1" ] 
+then
+	sec=0
+else
+	sec=$1
+fi
+
+
+rosbag play ${HOME}/bag_files/kitti/kitti_odometry_sequence_00.bag 	kitti/camera_color_right/image:=/camera/right/image_rect kitti/camera_color_left/image:=/camera/left/image_rect kitti/camera_color_right/camera_info:=/camera/right/camera_info kitti/camera_color_left/camera_info:=/camera/left/camera_info kitti/camera_gray_left/image:=/camera/left/image_rect_gray -s${sec}
