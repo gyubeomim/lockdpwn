2748d1bc503c8cd6d6db6d3082fd5c76e39ea681
edward0im <gyurse@gmail.com>
2020-01-07 20:06:01 +0900

    20200107 : Ubuntu 16.04.6 LTS

--- script_archive/run_orbslam.sh
+++ script_archive/run_orbslam.sh
@@ -13,11 +13,11 @@ case $1 in
 		${ORB_HOME}/Examples/Stereo/stereo_kitti ${HOME}/gitrepo/ORB_SLAM2/Vocabulary/ORBvoc.txt ${HOME}/catkin_ws/config/orb/stereo_KITTI00-02.yaml ${HOME}/datasets/kitti/data_odometry/sequences/00
 		;;
 	mros)
-		rosrun ORB_SLAM2 Mono ${HOME}/gitrepo/ORB_SLAM2/Vocabulary/ORBvoc.txt ${HOME}/catkin_ws/config/orb/KITTI00-02.yaml /camera/image_raw:=/camera/left/image_rect
+		${ORB_HOME}/Examples/ROS/ORB_SLAM2/Mono ${HOME}/gitrepo/ORB_SLAM2/Vocabulary/ORBvoc.txt ${HOME}/catkin_ws/config/orb/KITTI00-02.yaml /camera/image_raw:=/camera/left/image_rect
 		;;
 	sros)
 		ROS_NAMESPACE=camera rosrun stereo_image_proc stereo_image_proc &
 
-		rosrun ORB_SLAM2 Stereo ${HOME}/gitrepo/ORB_SLAM2/Vocabulary/ORBvoc.txt ${HOME}/catkin_ws/config/orb/atlas_stereo.yaml 0 /camera/left/image_raw:=/camera/left/image_rect /camera/right/image_raw:=/camera/right/image_rect
+		${ORB_HOME}/Examples/ROS/ORB_SLAM2/Stereo ${HOME}/gitrepo/ORB_SLAM2/Vocabulary/ORBvoc.txt ${HOME}/catkin_ws/config/orb/atlas_stereo.yaml 0 /camera/left/image_raw:=/camera/left/image_rect /camera/right/image_raw:=/camera/right/image_rect
 		;;
 esac
