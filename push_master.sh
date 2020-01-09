d30849e6e2e7b812e8b8385c60d14d450b67ef64
edward0im <gyurse@gmail.com>
2020-01-09 21:03:02 +0900

    20200109 :

--- push_master.sh
+++ push_master.sh
@@ -1,8 +1,18 @@
 git add . -A
-if [ -z "$1" ]
-then
-	git commit -m "$(date +%Y%m%d) : $(lsb_release -sd)" 
+
+if [[ "$OSTYPE" == "darwin19.0" ]]; then
+	if [ -z "$1" ]
+	then
+		git commit -m "$(date +%Y%m%d) : OSX" 
+	else
+		git commit -m "$1" 
+	fi
 else
-	git commit -m "$1" 
+	if [ -z "$1" ]
+	then
+		git commit -m "$(date +%Y%m%d) : $(lsb_release -sd)" 
+	else
+		git commit -m "$1" 
+	fi
 fi
 git push origin master
