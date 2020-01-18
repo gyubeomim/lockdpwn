a584302d1975c41734f7beb8d3d1ec1120da7b9a
edward <gyurse@gmail.com>
2020-01-18 13:10:23 +0900

    20200118 : Ubuntu 16.04.6 LTS

--- shell_scripts/run_dso.sh
+++ shell_scripts/run_dso.sh
@@ -24,22 +24,13 @@ case $1 in
 esac
 
 executable="${DSO_PATH}/build/bin/dso_dataset \
-			files=/media/data/dataset/kitti/data_odometry/sequences/00/image_0 \
+			files=${HOME}/dataset/kitti/data_odometry/sequences/00/image_0 \
 			calib=${HOME}/catkin_ws/config/dso/camera_kitti.txt \
 			gamma=${HOME}/catkin_ws/config/dso/pcalib.txt \
 			vignette=${HOME}/catkin_ws/config/dso/vignette.png \
 			preset=0 \
 			speed=1 \
 			mode=1"
-
-executable_mac="${DSO_PATH}/build/bin/dso_dataset \
-	files=${HOME}/dataset/kitti/data_odometry/sequences/00/image_0 \
-	calib=${HOME}/catkin_ws/config/dso/camera_kitti.txt \
-	gamma=${HOME}/catkin_ws/config/dso/pcalib.txt \
-	vignette=${HOME}/catkin_ws/config/dso/vignette.png \
-	preset=0 \
-	speed=1 \
-	mode=1"
 
 case $1 in
 	r)
@@ -52,9 +43,6 @@ case $1 in
 		;;
 	d)
 		gdb --ex="set print thread-events off" --ex="r" -args ${executable}
-		;;
-	rmac)
-		$executable_mac
 		;;
 esac
 # set speed=1 to play with real-time. otherwise, it plays as fast as possible.
