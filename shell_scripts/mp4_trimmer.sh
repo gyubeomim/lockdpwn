26f9ad20af07c4bbff195d6ca54d440c73df0d27
edward0im <gyurse@gmail.com>
2020-01-19 14:27:29 +0900

    20200119 : Ubuntu 16.04.6 LTS

--- shell_scripts/mp4_trimmer.sh
+++ shell_scripts/mp4_trimmer.sh
@@ -1,7 +1,1 @@
-if [ ! -z "$1" ] 
-then
-	ffmpeg -i $1 -ss $2 -to $3 -c copy ${HOME}/Downloads/output-$(date +%Y%m%d_%H%M).mp4
-	echo "[+] Trimmed file is saved in ~/Downloads/"
-else
-	echo "[-] No Input argument is contained! use ./mp4_compressor.sh {filename} {from} {to}, format: 00:00:00"
-fi
+/home/atlas1/gitrepo/lockdpwn/shell_scripts/mp4_compressor.sh
