3af790f269459c3cf22a698503e72b11d9f43de1
edward0im <gyurse@gmail.com>
2020-02-07 17:49:17 +0900

    20200207 : OSX

--- cmake_projects/vtk_examples/src/HighlightSelectedPoints.cxx
+++ cmake_projects/vtk_examples/src/HighlightSelectedPoints.cxx
@@ -33,7 +33,7 @@ class InteractorStyle : public vtkInteractorStyleRubberBandPick {
     this->SelectedActor->SetMapper(SelectedMapper);
   }
 
-  virtual void OnLeftButtonUp() {
+  virtual void OnLeftButtonUp() override {
     // Forward events
     vtkInteractorStyleRubberBandPick::OnLeftButtonUp();
 
@@ -42,7 +42,32 @@ class InteractorStyle : public vtkInteractorStyleRubberBandPick {
     vtkSmartPointer<vtkExtractGeometry> extractGeometry = vtkSmartPointer<vtkExtractGeometry>::New();
 
     extractGeometry->SetImplicitFunction(frustum);
+    extractGeometry->SetInputData(this->Points);
+    extractGeometry->Update();
 
+    vtkSmartPointer<vtkVertexGlyphFilter> glyphFilter = vtkSmartPointer<vtkVertexGlyphFilter>::New();
+    glyphFilter->SetInputConnection(extractGeometry->GetOutputPort());
+    glyphFilter->Update();
+
+    vtkPolyData* selected = glyphFilter->GetOutput();
+    std::cout << "Selected " << selected->GetNumberOfPoints() << " points." << std::endl;
+    std::cout << "Selected " << selected->GetNumberOfCells() << " cells." << std::endl;
+
+    this->SelectedMapper->SetInputData(selected);
+    this->SelectedMapper->ScalarVisibilityOff();
+
+    vtkIdTypeArray* ids = vtkIdTypeArray::SafeDownCast(selected->GetPointData()->GetArray("OriginalIds"));
+
+    for(vtkIdType i=0; i < ids->GetNumberOfTuples(); i++) {
+      std::cout << "Id " << i << " : " << ids->GetValue(i) << std::endl;
+    }
+
+    this->SelectedActor->GetProperty()->SetColor(1.0, 0.0, 0.0); // RGB
+    this->SelectedActor->GetProperty()->SetPointSize(3);
+
+    this->CurrentRenderer->AddActor(SelectedActor);
+    this->GetInteractor()->GetRenderWindow()->Render();
+    this->HighlightProp(NULL);
   }
 
   void SetPoints(vtkSmartPointer<vtkPolyData> points) { this->Points = points; }
@@ -55,7 +80,48 @@ class InteractorStyle : public vtkInteractorStyleRubberBandPick {
 vtkStandardNewMacro(InteractorStyle);
 
 int main(int argc, char **argv) {
+  vtkSmartPointer<vtkPointSource> pointSource = vtkSmartPointer<vtkPointSource>::New();
+  pointSource->SetNumberOfPoints(20);
+  pointSource->Update();
 
+  vtkSmartPointer<vtkIdFilter> idFilter = vtkSmartPointer<vtkIdFilter>::New();
+  idFilter->SetInputConnection(pointSource->GetOutputPort());
+  idFilter->SetIdsArrayName("OriginalIds");
+  idFilter->Update();
+
+  vtkSmartPointer<vtkDataSetSurfaceFilter> surfaceFilter =  vtkSmartPointer<vtkDataSetSurfaceFilter>::New();
+  surfaceFilter->SetInputConnection(idFilter->GetOutputPort());
+  surfaceFilter->Update();
+
+  vtkPolyData* input = surfaceFilter->GetOutput();
+
+  // create a mapper and actor.
+  vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper->SetInputData(input);
+  mapper->ScalarVisibilityOff();
+
+  vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
+
+  // visualize.
+  vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+
+  vtkSmartPointer<vtkAreaPicker> areaPicker = vtkSmartPointer<vtkAreaPicker>::New();
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  renderWindowInteractor->SetPicker(areaPicker);
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+
+  renderer->AddActor(actor);
+  // renderer->SetBackground(1, 1, 1);
+
+  renderWindow->Render();
+
+  vtkSmartPointer<InteractorStyle> style = vtkSmartPointer<InteractorStyle>::New();
+  style->SetPoints(input);
+  renderWindowInteractor->SetInteractorStyle(style);
+  renderWindowInteractor->Start();
 
   return 0;
 }
