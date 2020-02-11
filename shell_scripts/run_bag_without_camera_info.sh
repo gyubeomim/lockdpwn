05928496a6f7bb807cea98f012168e6546fff4be
edward0im <gyurse@gmail.com>
2020-02-11 19:23:35 +0900

    20200211 : Ubuntu 16.04.6 LTS

--- shell_scripts/run_bag_without_camera_info.sh
+++ shell_scripts/run_bag_without_camera_info.sh
@@ -1,5 +1,12 @@
 if [ -z "$1" ]; then
 	echo "Usage: run_bag_without_camera_info.sh {bagfile}"
 else
-	rosbag play $1 --topic /camera/left/image_raw /camera/right/image_raw -r0.2 -l -s14
+	case $2 in
+		1)
+			rosbag play $1 --topic /camera/left/image_raw /camera/right/image_raw -r0.1 -l -s14 -u1
+			;;
+		*)
+			rosbag play $1 --topic /camera/left/image_raw /camera/right/image_raw -r0.2 -l
+			;;
+	esac
 fi
