1aee4a22039992ae47750750275c297cd8e71dac
edward im <gyurse@gmail.com>
2020-03-14 23:33:19 +0900

    20200314 : Ubuntu 16.04.6 LTS

--- docker/lsd_slam/run.sh
+++ docker/lsd_slam/run.sh
@@ -1,21 +1,41 @@
 #!/bin/sh
 XSOCK=/tmp/.X11-unix
-XAUTH=/home/${USER}/.Xauthority
+XAUTH=${HOME}/.Xauthority
 
 xhost +local:docker
 
-docker run \
-	--runtime=nvidia \
-	--net=host \
-	--name lsdslam \
-    -it \
-	--env="XAUTHORITY=${XAUTH}" \
-	--env="DISPLAY=unix${DISPLAY}" \
-	--env="XDG_RUNTIME_DIR=/run/user/1000" \
-	--privileged  \
-	-v /run/user/1000:/run/user/1000 \
-	-v /dev/bus/usb:/dev/bus/usb \
-	-v ${XSOCK}:${XSOCK}:rw \
-	-v ${XAUTH}:${XAUTH}:rw \
-	edward0im/starlaw:lsd-slam \
-	/bin/bash
+case $1 in
+	cpu)
+		docker run \
+			--net=host \
+			--name lsdslam \
+			-it \
+			--env="XAUTHORITY=${XAUTH}" \
+			--env="DISPLAY=unix${DISPLAY}" \
+			--env="XDG_RUNTIME_DIR=/run/user/1000" \
+			--privileged  \
+			-v /run/user/1000:/run/user/1000 \
+			-v /dev/bus/usb:/dev/bus/usb \
+			-v ${XSOCK}:${XSOCK}:rw \
+			-v ${XAUTH}:${XAUTH}:rw \
+			edward0im/starlaw:lsd-slam \
+			/bin/bash
+		;;
+	*)
+		docker run \
+			--runtime=nvidia \
+			--net=host \
+			--name lsdslam \
+			-it \
+			--env="XAUTHORITY=${XAUTH}" \
+			--env="DISPLAY=unix${DISPLAY}" \
+			--env="XDG_RUNTIME_DIR=/run/user/1000" \
+			--privileged  \
+			-v /run/user/1000:/run/user/1000 \
+			-v /dev/bus/usb:/dev/bus/usb \
+			-v ${XSOCK}:${XSOCK}:rw \
+			-v ${XAUTH}:${XAUTH}:rw \
+			edward0im/starlaw:lsd-slam \
+			/bin/bash
+		;;
+esac
--- docker/lsd_slam/run_lsdslam.sh
+++ docker/lsd_slam/run_lsdslam.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 XSOCK=/tmp/.X11-unix
-XAUTH=/home/${USER}/.Xauthority
+XAUTH=${HOME}/.Xauthority
 
 xhost +local:docker
 
-	# --runtime=nvidia \
+case $1 in 
+	cpu)
 docker run \
 	--net=host \
 	--name lsdslam \
@@ -20,3 +21,23 @@ docker run \
 	-v ${XAUTH}:${XAUTH}:rw \
 	edward0im/starlaw:lsd-slam \
 	/root/run_lsdslam.sh
+	;;
+*)
+docker run \
+	--runtime=nvidia \
+	--net=host \
+	--name lsdslam \
+	--rm \
+    -it \
+	--env="XAUTHORITY=${XAUTH}" \
+	--env="DISPLAY=unix${DISPLAY}" \
+	--env="XDG_RUNTIME_DIR=/run/user/1000" \
+	--privileged  \
+	-v /run/user/1000:/run/user/1000 \
+	-v /dev/bus/usb:/dev/bus/usb \
+	-v ${XSOCK}:${XSOCK}:rw \
+	-v ${XAUTH}:${XAUTH}:rw \
+	edward0im/starlaw:lsd-slam \
+	/root/run_lsdslam.sh
+	;;
+	esac
