584af48399b0b3bd96d76e8fb8245662023d64c1
edward <gyurse@gmail.com>
2020-04-18 17:55:01 +0900

    20200418 : Ubuntu 16.04.6 LTS

--- shell_scripts/run_kittibag.sh
+++ shell_scripts/run_kittibag.sh
@@ -20,7 +20,7 @@ case $2 in
 			kitti/camera_color_right/image:=/camera/right/image_rect \
 			kitti/camera_color_left/image:=/camera/left/image_rect \
 			kitti/camera_color_right/camera_info:=/camera/right/camera_info \
-			kitti/camera_color_left/camera_info:=/camera/left/camera_info \ 
+			kitti/camera_color_left/camera_info:=/camera/left/camera_info \
 			kitti/camera_gray_left/image:=/camera/left/image_rect_gray -s${sec}
 		;;
 esac
